bf4e9958834476990d7fed3923cc48d535d76717
kumard3 <kumardeepanshu157200@gmail.com>
2021-09-22 14:24:35 +0530

    mongodb added

--- config/database.js
+++ config/database.js
@@ -2,13 +2,18 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: 'mongoose',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        host: env('DATABASE_HOST', 'cluster0.seq2f.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'astroyantra-blog-cms'),
+        username: env('DATABASE_USERNAME', 'cyphen'),
+        password: env('DATABASE_PASSWORD', 'cyphen123'),
       },
       options: {
-        useNullAsDefault: true,
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
